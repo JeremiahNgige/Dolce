e811665e259f361a392163ade2ed1ddc77d5750a
Jeremiah Brian <brianjeremy123789@gmail.com>
2020-07-30 21:47:39 +0300

    col-imgs

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 100) {
     document.getElementById("navTop").style.background = "black";
-    document.getElementById("navTop").style.padding = "5px";
+    document.getElementById("navTop").style.padding = "15px";
      
   } else {
     document.getElementById("navTop").style.background = "rgba(0, 0, 0, 0.196)";
